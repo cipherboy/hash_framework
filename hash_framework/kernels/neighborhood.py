f4897a3a2e863973871fff206f5b24662faea4ff
Alexander Scheel <alexander.m.scheel@gmail.com>
2017-12-09 00:42:25 -0600

    Fix neighborhood
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- hash_framework/kernels/neighborhood.py
+++ hash_framework/kernels/neighborhood.py
@@ -147,7 +147,6 @@ class Neighborhood(Kernel):
         return d
 
     def on_result(algo, db, tags, work, wid, result):
-        tag = tags[wid]
         if type(result['results']) == list and len(result['results']) > 0:
             algo.rounds = work[wid][0]
             attacks.collision.insert_db_multiple_automatic_tag(algo, db, result['results'], False)
