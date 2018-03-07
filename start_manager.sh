c8d25ab0f75a2bb0a2e72f2df7e76d6924a84258
Alexander Scheel <alexander.m.scheel@gmail.com>
2018-03-07 10:41:16 -0600

    Update scripts
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- start_manager.sh
+++ start_manager.sh
@@ -2,4 +2,9 @@
 
 py_gunicorn=$(which gunicorn)
 
-$py_gunicorn hash_framework.manager.__main__:app --workers=1 $@
+if [ "$py_gunicorn" == "" ]; then
+    echo "No gunicorn found" 1>&2
+    exit 1
+fi
+
+$py_gunicorn hash_framework.manager.__main__:app --workers 1 --backlog 8192 --bind '0.0.0.0:8000' $@
--- start_workers.sh
+++ start_workers.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 num_process=`cat /proc/cpuinfo | grep 'processor' | wc -l`
-min_port=5000
-max_port=$((min_port + num_process - 1))
 
 bash ./stop_workers.sh
 mkdir -p ~/logs
 rm -rf ~/models ~/kernel_cache
 mkdir -p ~/models ~/kernel_cache ~/results
 
-python3 -m hash_framework.workers db
-
 for port in `seq $min_port $max_port`; do
     echo "Starting..."
-    python3 -m hash_framework.workers $port 2> ~/logs/worker-$port-err.log >~/logs/worker-$port.log &
+    python3 -m hash_framework.workers $@ 2> ~/logs/worker-$port-err.log >~/logs/worker-$port.log &
     pid="$!"
     echo "PID: $pid"
     echo "$pid" > ~/logs/worker-$port.pid
