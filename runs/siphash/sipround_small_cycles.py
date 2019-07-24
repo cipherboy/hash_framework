3383230ad404ee08af26b4677742e23843d823a7
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-07-24 08:17:59 -0400

    Add sipround_small_cycles.py
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

---
+++ runs/siphash/sipround_small_cycles.py
@@ -0,0 +1,33 @@
+import cmsh
+import hash_framework.algorithms._siphash as _siphash
+
+def main():
+    m = cmsh.Model()
+    v0 = m.vec(64)
+    v1 = m.vec(64)
+    v2 = m.vec(64)
+    v3 = m.vec(64)
+
+    r0, r1, r2, r3 = _siphash.sipround(v0, v1, v2, v3)
+    o0, o1, o2, o3 = _siphash.sipround(r0, r1, r2, r3)
+
+    m.add_assert(v0 == o0)
+    m.add_assert(v1 == o1)
+    m.add_assert(v2 == o2)
+    m.add_assert(v3 == o3)
+
+    sat = m.solve()
+    assert sat
+
+    while sat:
+        print(hex(int(v0)), hex(int(v1)), hex(int(v2)), hex(int(v3)))
+        joined = m.join_vec([v0, v1, v2, v3])
+        negated = m.negate_solution(joined)
+        m.add_assert(negated)
+        sat = m.solve()
+
+
+
+if __name__ == "__main__":
+    main()
+
