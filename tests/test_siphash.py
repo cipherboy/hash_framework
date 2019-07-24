d3d8c985251b982a80deba74f66175936b7336de
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-07-24 08:11:09 -0400

    Update siphash test case
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- tests/test_siphash.py
+++ tests/test_siphash.py
@@ -1,5 +1,5 @@
 import cmsh
-from hash_framework.algorithms._siphash import *
+import hash_framework.algorithms.siphash as siphash
 
 def gen_blocks(model, count, width):
     result = []
@@ -19,19 +19,20 @@ def split_hex(string, width=8):
 
 def test_known_value():
     model = cmsh.Model()
+    hf = siphash()
 
     key = model.to_vec(0x00, 128)
     block = []
-    assert int(siphash(model, key, block)) == 0x1e924b9d737700d7
+    assert int(hf.compute(model, key, block)) == 0x1e924b9d737700d7
 
     key = model.to_vec(0x000102030405060708090a0b0c0d0e0f, 128)
     block = model.to_vec(0x000102030405060708090a0b0c0d0e, 120)
-    assert int(siphash(model, key, block)) == 0xa129ca6149be45e5
+    assert int(hf.compute(model, key, block)) == 0xa129ca6149be45e5
 
     key = model.to_vec(0x00, 128)
     block = model.to_vec(0x48656c6c6f20776f726c64, 88)
-    assert int(siphash(model, key, block)) == 0xc9e8a3021f3822d9
+    assert int(hf.compute(model, key, block)) == 0xc9e8a3021f3822d9
 
     key = model.to_vec(0x00, 128)
     block = model.to_vec(0x3132333435363738313233, 88)
-    assert int(siphash(model, key, block)) == 0xf95d77ccdb0649f
+    assert int(hf.compute(model, key, block)) == 0xf95d77ccdb0649f
