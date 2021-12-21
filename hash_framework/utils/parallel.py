f278919a5d510642c3a050d5b70147c8bf832ece
Alexander Scheel <alexander.m.scheel@gmail.com>
2021-12-20 21:09:36 -0500

    Update parallel implementation to allow returns
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- hash_framework/utils/parallel.py
+++ hash_framework/utils/parallel.py
@@ -6,20 +6,40 @@ def block_until_slots(queue, max_threads, granularity):
     while all(map(lambda x: not x.ready(), queue)) and len(queue) >= max_threads:
         time.sleep(granularity)
 
-    return list(filter(lambda x: not x.ready(), queue))
+    new_results = []
+    for proc in queue:
+        if proc.ready():
+            new_results.append(proc.get())
+
+    return list(filter(lambda x: not x.ready(), queue)), new_results
 
 
 def parallel_run(
-    func, generator, processes=multiprocessing.cpu_count(), granularity=0.1
+    func, generator, processes=multiprocessing.cpu_count(), granularity=0.1, items_to_process=None
 ):
     pool = multiprocessing.Pool(processes)
     proc_queue: list = []
+    results = []
 
-    for item in generator():
-        proc_queue = block_until_slots(proc_queue, processes, granularity)
+    if callable(generator):
+        generator = generator()
+
+    processed = 0
+
+    for item in generator:
+        proc_queue, new_results = block_until_slots(proc_queue, processes, granularity)
+        results.extend(new_results)
         new_proc = pool.apply_async(func, args=item, error_callback=print)
         proc_queue.append(new_proc)
+        processed += 1
+        if processed == items_to_process and items_to_process is not None:
+            break
 
     pool.close()
     pool.join()
-    return None
+
+    for proc in proc_queue:
+        if proc.ready():
+            results.append(proc.get())
+
+    return results
